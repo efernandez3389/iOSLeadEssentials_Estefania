fe9c9fe83a7f8b6149b4e878fb065a42a6ba112a
Estefania Fernandez <estefaniafer89@gmail.com>
2024-06-08 15:43:12 +0200

    Move ViewModels to separate file

---
+++ EssentialFeed/EssentialFeediOS/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Estefania Fernandez on 8/6/24.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Estefania Fernandez on 8/6/24.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
