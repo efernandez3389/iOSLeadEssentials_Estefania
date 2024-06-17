313e60aa759b174dacc8b9dcef7f1c5f9b1c968f
Estefania Fernandez <estefaniafer89@gmail.com>
2024-06-17 10:59:07 +0200

    Move ErrorView to separate file

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Estefania Fernandez on 17/6/24.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
