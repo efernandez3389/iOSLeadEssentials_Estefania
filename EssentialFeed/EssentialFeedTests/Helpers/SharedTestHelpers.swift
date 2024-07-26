6d3272531babb6bc3ea7fe3e271edc9ac86683cd
Estefania Fernandez <estefaniafer89@gmail.com>
2024-07-26 20:10:32 +0200

    Test ImageCommentsMapper in isolation

--- EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -19,12 +19,12 @@ func anyData() -> Data {
     return Data("any data".utf8)
 }
 
-private func makeItemsJSON(_ items: [[String: Any]]) -> Data{
+func makeItemsJSON(_ items: [[String: Any]]) -> Data{
     let json = ["items": items]
     return try! JSONSerialization.data(withJSONObject: json)
 }
 
-private extension HTTPURLResponse {
+extension HTTPURLResponse {
     convenience init(statusCode: Int) {
         self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
     }
