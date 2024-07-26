dee9b7bb5489194fcb07c309d7766e4251fdb01d
Estefania Fernandez <estefaniafer89@gmail.com>
2024-07-26 19:56:18 +0200

    Move test helpers to shared scope

--- EssentialFeed/EssentialFeedTests/Feed API/FeedItemMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedItemMapperTests.swift
@@ -1,5 +1,5 @@
 //
-//  RemoteFeedLoaderTest.swift
+//  FeedItemMapperTests.swift
 //  EssentialFeedTests
 //
 //  Created by Estefania Fernandez on 5/5/24.
@@ -78,14 +78,5 @@ class FeedItemMapperTests: XCTestCase {
         return (item, json)
     }
     
-    private func makeItemsJSON(_ items: [[String: Any]]) -> Data{
-        let json = ["items": items]
-        return try! JSONSerialization.data(withJSONObject: json)
-    }
-}
-
-private extension HTTPURLResponse {
-    convenience init(statusCode: Int) {
-        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
-    }
+    
 }
