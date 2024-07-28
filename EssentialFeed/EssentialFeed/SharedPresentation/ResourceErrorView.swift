679060b9654eebfdc3963be164266ea4ee929416
Estefania Fernandez <estefaniafer89@gmail.com>
2024-07-28 12:45:57 +0200

    Rename and move ResourceerrorView and ViewModel to shared module

---
+++ EssentialFeed/EssentialFeed/SharedPresentation/ResourceErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceErrorView.swift
+//  EssentialFeed
+//
+//  Created by Estefania Fernandez on 28/7/24.
+//
+
+import Foundation
+
+public protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}
